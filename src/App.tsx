d9e290d779db0836f95679c401a59e28231f7ad8
Naseel Niyas <codenas07@gmail.com>
2023-06-26 12:47:22 +0300

    Tutorial Completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,30 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import axios from 'axios'
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [value, setValue] = useState("")
 
+  async function onSubmit(e: any) {
+    e.preventDefault()
+    const {data} = await axios.get(
+      'https://techharvestingfunctionapp.azurewebsites.net/api/mynamefunction?code=Ar0JEgKh0CPj_msRFISL-YqfUpMKwpexLnMkdz8Qn0pqAzFu1JWR-w==', {
+        params: {
+          name: value
+        }
+      }
+    );
+    console.log(data)
+    alert(data)
+  }
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <form onSubmit={onSubmit}>
+        <input type="text"  value={value} onChange={(e) => setValue(e.target.value)} placeholder='Name'/>
+        <button>Submit</button>
+
+
+      </form>
     </>
   )
 }
